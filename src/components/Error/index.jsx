ecc3e74f001fdc65f400bc29596f6189231f8259
Subham Panda <me.subham01@gmail.com>
2024-05-28 10:18:11 +0000

    link added for error element

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 const Error = () => {
   const error = useRouteError();
   console.log(error);
@@ -9,7 +9,9 @@ const Error = () => {
         <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Uh-oh!
         </h1>
-        <p className="mt-4 text-gray-500">We can't find that page.</p>
+        <p className="mt-4 text-gray-500">
+          We can't find that page. <Link>Dashboard</Link>
+        </p>
       </div>
     </div>
   );
